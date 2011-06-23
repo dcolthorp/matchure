0628e9a5a31052fafb5722c4e365efd1cb8b75cb
Ryan Senior <rsenior@revelytix.com>
2011-06-23 10:15:34 -0500

    updated to work with clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject matchure "0.10.0"
   :description "Idiomatic and powerful pattern matching in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
