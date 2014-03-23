c8e348f001ed6024ab339a215666f90a63a7dd47
Justiniac <justiniac@undergroundtechno.com>
2014-03-23 16:19:36 +0100

    Promote to release 0.13.0

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject matchure "0.13.0-SNAPSHOT"
+(defproject org.clojars.justiniac/matchure "0.13.0"
   :description "Idiomatic and powerful pattern matching in clojure"
+  :url "https://github.com/justiniac/matchure"
+  :license {:name "Copyright (c) 2011 Drew Colthorp"
+            :url "https://github.com/justiniac/matchure#license"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.macro "0.1.2"]])
