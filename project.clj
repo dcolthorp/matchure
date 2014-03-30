75405c876dbba5be38058a258da729a5a72c2f22
Justiniac <fdeserres@palo-it.com>
2014-03-30 17:25:25 +0200

    Clojure 1.6

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.justiniac/matchure "0.13.0"
+(defproject org.clojars.justiniac/matchure "0.13.1"
   :description "Idiomatic and powerful pattern matching in clojure"
   :url "https://github.com/justiniac/matchure"
   :license {:name "Copyright (c) 2011 Drew Colthorp"
             :url "https://github.com/justiniac/matchure#license"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.macro "0.1.2"]])
